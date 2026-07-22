009d3cbd2a7f3a6f6bbfd53340fc3d579986384f
Zubair-techbeeshub <zubair.siddiqui@techbeeshub.com>
2026-07-22 13:49:07 +0530

    Update helloworld.py

--- FAST API/helloworld.py
+++ FAST API/helloworld.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 @app.get('/')
-async def index():
+async def index() -> dict[str,str]:
     return {'hello':'world'}
 
 @app.get('/about')
-async def about():
+async def about() -> dict [str,str]:
     return {'about':'This is an IT Company'}
